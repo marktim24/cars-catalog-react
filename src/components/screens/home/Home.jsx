85ef786668582544f076cca69a48056fb8f010bb
marktim24 <timoshevich199524@mail.ru>
2023-03-17 17:43:06 +0200

    Filter added

--- src/components/screens/home/Home.jsx
+++ src/components/screens/home/Home.jsx
@@ -1,30 +1,15 @@
+import { useMemo } from 'react'
+import CarItem from './car-item/CarItem.jsx'
 import { cars } from './cars.data.js'
-import styles from './Home.module.scss'
 
 const Home = () => {
+	const filterCars = useMemo(() => cars.filter(car => car.price < 20000), [])
 	return (
 		<div>
 			<h1>Cars catalog</h1>
 			<div>
-				{cars.length ? (
-					cars.map(car => (
-						<div key={car.id} className={styles.item}>
-							<div
-								className={styles.image}
-								style={{ backgroundImage: `url(${car.image}` }}
-							></div>
-							<div className={styles.info}>
-								<h2>{car.name}</h2>
-								<p>
-									{new Intl.NumberFormat('ru-RU', {
-										style: 'currency',
-										currency: 'EUR',
-									}).format(car.price)}
-								</p>
-								<button>Purchase</button>
-							</div>
-						</div>
-					))
+				{filterCars.length ? (
+					filterCars.map(car => <CarItem key={car.id} car={car} />)
 				) : (
 					<p>There are no cars available(</p>
 				)}
