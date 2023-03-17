9eeb4aff9b9acb8086e777033371df20d2ab39f3
marktim24 <timoshevich199524@mail.ru>
2023-03-17 17:20:51 +0200

    Car items done

--- src/components/screens/home/Home.jsx
+++ src/components/screens/home/Home.jsx
@@ -1,18 +1,33 @@
+import { cars } from './cars.data.js'
 import styles from './Home.module.scss'
 
 const Home = () => {
 	return (
 		<div>
 			<h1>Cars catalog</h1>
-			<div className={styles.item}>
-				<div className={styles.image}>
-					<img src='/car.jpg' alt='' />
-					<div className={styles.info}>
-						<h2>Car Name</h2>
-						<p>Price</p>
-						<button>More</button>
-					</div>
-				</div>
+			<div>
+				{cars.length ? (
+					cars.map(car => (
+						<div key={car.id} className={styles.item}>
+							<div
+								className={styles.image}
+								style={{ backgroundImage: `url(${car.image}` }}
+							></div>
+							<div className={styles.info}>
+								<h2>{car.name}</h2>
+								<p>
+									{new Intl.NumberFormat('ru-RU', {
+										style: 'currency',
+										currency: 'EUR',
+									}).format(car.price)}
+								</p>
+								<button>Purchase</button>
+							</div>
+						</div>
+					))
+				) : (
+					<p>There are no cars available(</p>
+				)}
 			</div>
 		</div>
 	)
---
+++ src/components/screens/home/cars.data.js
@@ -0,0 +1,16 @@
+export const cars = [
+	{
+		id: 1,
+		name: 'BMW i8',
+		price: 99490,
+		image:
+			'https://i-electromobile.ru/wp-content/uploads/2021/07/BMW-i8-Hibrid.jpg',
+	},
+	{
+		id: 2,
+		name: 'Toyota Camry',
+		price: 14990,
+		image:
+			'https://www.warnaplus.com/wp-content/uploads/2021/11/Foto-1-9-1536x868.jpg',
+	},
+]
